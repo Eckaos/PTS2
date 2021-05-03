5f1eab651f6e6a7d8fc040af7d5989e67bf8fe4f
Nathan POLLART <nathan.pollart@hotmail.fr>
2021-05-03 13:03:14 +0200

    modification des noms

--- StudentApp/src/application/Main.java
+++ StudentApp/src/application/Main.java
@@ -20,7 +20,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Accueil.fxml"));
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("GUIStudent.fxml"));
+			screens.add((AnchorPane)FXMLLoader.load(getClass().getResource("Accueil.fxml")));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
