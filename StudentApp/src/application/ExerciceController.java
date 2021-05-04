2d47aafded20276b048beb4be8b0cb30c97d2313
Nathan POLLART <nathan.pollart@hotmail.fr>
2021-05-04 11:46:48 +0200

    media view

--- StudentApp/src/application/ExerciceController.java
+++ StudentApp/src/application/ExerciceController.java
@@ -1,5 +1,22 @@
 package application;
 
-public class ExerciceController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.Initializable;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+public class ExerciceController implements Initializable{
+
+	MediaView testMe = new MediaView(new MediaPlayer(new Media(null)));
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
--- StudentApp/src/application/Main.java
+++ StudentApp/src/application/Main.java
@@ -6,25 +6,29 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	
-	static BorderPane root;
-	static List<AnchorPane> screens = new ArrayList<>();
+	static Parent root;
+	static List<BorderPane> screens = new ArrayList<>();
 	static int currentIndex;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			root = (BorderPane)FXMLLoader.load(getClass().getResource("GUIStudent.fxml"));
-			Scene scene = new Scene(root,400,400);
+			root = FXMLLoader.load(getClass().getResource("GUIStudent.fxml"));
+			screens.add((BorderPane)FXMLLoader.load(getClass().getResource("Accueil.fxml")));
+			//root.getChildren().add(screens.get(0));
+			Scene scene = new Scene(screens.get(0),1280,720);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setMaximized(true);
+			primaryStage.setMinHeight(720);
+			primaryStage.setMinWidth(1280);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -36,13 +40,11 @@ public class Main extends Application {
 		launch(args);
 	}
 	
-	public static AnchorPane getScreen(int index) {
+	public static BorderPane getScreen(int index) {
 		return screens.get(index);
 	}
 	
 	public static void setScreen(int index) {
-		root.getChildren().remove(screens.get(currentIndex));
-		root.getChildren().add(screens.get(index));
 		currentIndex = index;
 	}
 }
