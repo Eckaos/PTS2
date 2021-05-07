103bfd68dc88a3347f7eff134813a29c067919e6
Nathan POLLART <nathan.pollart@hotmail.fr>
2021-05-07 20:26:09 +0200

    add

--- ProfessorApp/src/application/AccueilController.java
+++ ProfessorApp/src/application/AccueilController.java
@@ -1,5 +1,26 @@
 package application;
 
-public class AccueilController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class AccueilController implements Initializable{
+
+	@FXML
+	Button createModifyButton;
+	
+	@FXML
+	Button result;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		createModifyButton.setOnAction(ActionEvent -> Main.setScreen(1));
+		result.setOnAction(ActionEvent -> Main.setScreen(2));
+		
+	}
 	
 }
