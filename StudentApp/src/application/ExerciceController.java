ec6b81621fd33bf71fd65ed7de293a6c079a612c
Nathan POLLART <nathan.pollart@hotmail.fr>
2021-05-07 18:56:19 +0200

    add video test

--- StudentApp/src/application/ExerciceController.java
+++ StudentApp/src/application/ExerciceController.java
@@ -16,13 +16,15 @@ public class ExerciceController implements Initializable{
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		File pathString = new File("src/Avatar.avi");
+		File pathString = new File("src/vagos.mp4");
 		
 		try {
 			testMe.setMediaPlayer(new MediaPlayer(new Media(pathString.toURI().toURL().toExternalForm())));
+			testMe.getMediaPlayer().setAutoPlay(true);
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 	}
 }
