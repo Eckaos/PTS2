d4d6bbdede9273a2548e573d24244d6b279bcc9f
Nathan POLLART <nathan.pollart@hotmail.fr>
2021-05-07 20:04:14 +0200

    media add

--- StudentApp/src/application/ExerciceController.java
+++ StudentApp/src/application/ExerciceController.java
@@ -7,12 +7,41 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.media.MediaView;
+import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
 import javafx.scene.media.*;
+import javafx.scene.media.MediaPlayer.Status;
 
 public class ExerciceController implements Initializable{
 
 	@FXML
 	MediaView testMe;
+	@FXML
+	Text instructionText;
+	@FXML
+	Button pausePlayButton;
+	@FXML
+	ProgressBar progressBar;
+	@FXML
+	Button soundButton;
+	@FXML
+	Button fullscreenButton;
+	
+	
+	@FXML
+	Text timeText;
+	@FXML
+	Button helpButton;
+	@FXML
+	Text textToFind;
+	@FXML
+	TextField typedText;
+	@FXML
+	Button validateButton;
+	@FXML
+	Button finishButton;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -25,6 +54,48 @@ public class ExerciceController implements Initializable{
 			e.printStackTrace();
 		}
 		
+		pausePlayButton.setOnAction(ActionEvent -> 
+			{
+				if (testMe.getMediaPlayer().getStatus().equals(Status.PAUSED)) {
+					testMe.getMediaPlayer().play();
+				}else {
+					testMe.getMediaPlayer().pause();
+				}
+			}
+		);
 		
+		soundButton.setOnAction(ActionEvent -> 
+		{
+			if (testMe.getMediaPlayer().isMute()) {
+				testMe.getMediaPlayer().setMute(false);
+			}else {
+				testMe.getMediaPlayer().setMute(true);
+			}
+			
+		}
+		);
+		
+		testMe.setOnMouseClicked(ActionEvent -> 
+			{
+				if (testMe.getMediaPlayer().getStatus().equals(Status.PAUSED)) {
+					testMe.getMediaPlayer().play();
+				}else {
+					testMe.getMediaPlayer().pause();
+				}
+			}
+		);
+		
+		//TODO recuperer le text depuis l'exercice
+		String text = null;
+		textToFind.setText(text);
+		
+		validateButton.setOnAction(ActionEvent -> verify(typedText.getText()));
+		
+		//TODO faire la popup
+		finishButton.setOnAction(null);
+	}
+
+	private void verify(String text) {
+		// TODO faire l'algorithme de découverte de mot
 	}
 }
